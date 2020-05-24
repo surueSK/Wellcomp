ccf696297bcf2dc7a2ad7bfbbe2dbedc3aa3ee95
surueSK <35964012+surueSK@users.noreply.github.com>
2020-05-24 17:23:38 +0900

    ADLogger_v2 作成
    簡単なストップウォッチアプリを作成 (StoryBoard配置が絶対値なので変更が必要)

---
+++ grad-pro/ADLogger_v2/ADLogger_v2Tests/ADLogger_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ADLogger_v2Tests.swift
+//  ADLogger_v2Tests
+//
+//  Created by 助川友理 on 2020/05/21.
+//  Copyright © 2020 助川友理. All rights reserved.
+//
+
+import XCTest
+@testable import ADLogger_v2
+
+class ADLogger_v2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
