d0e80a194c2e530666b60e126130c006520771da
yuri <memory.instrument@gmail.com>
2020-05-31 13:39:31 +0900

    メイン画面のアラート実装

--- grad-pro/ADLogger_v2/ADLogger_v2/ViewController.swift
+++ grad-pro/ADLogger_v2/ADLogger_v2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
  
 class ViewController: UIViewController {
  
+    @IBOutlet var timerButton: UIButton!
     @IBOutlet var timerHour: UILabel!
     @IBOutlet var timerMinute: UILabel!
     @IBOutlet var timerSecond: UILabel!
@@ -17,60 +18,57 @@ class ViewController: UIViewController {
     
     weak var timer: Timer!
     var startTime = Date()
+    var status: Bool = false
+    
+    @IBAction func timerCount(){
+    // MARK: - 分岐
+        
+        if (status) {
+            stop()
+        } else {
+            start()
+        }
+    }
+    
+    func start(){
+        timerButton.setTitle("STOP", for: .normal)
+        status = true
+    }
+    func stop(){
+        timerButton.setTitle("START", for: .normal)
+        status = false
+        
+    // MARK: - アラート
+        
+        let alert: UIAlertController = UIAlertController(title: "計測を終了しますか？", message: "計測を終了する場合は終了ボタンを押してタスク選択", preferredStyle:  UIAlertController.Style.alert)
+        
+        let defaultAction1: UIAlertAction = UIAlertAction(title: "終了", style: UIAlertAction.Style.default, handler:{
+               (action: UIAlertAction!) -> Void in
+               print("記録画面へ移行")
+           })
+        
+        let defaultAction2: UIAlertAction = UIAlertAction(title: "計測に戻る", style: UIAlertAction.Style.default, handler:{
+            (action: UIAlertAction!) -> Void in
+            print("計測に戻る")
+        })
+        
+           // キャンセルボタン
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Reset", style: UIAlertAction.Style.cancel, handler:{
+               (action: UIAlertAction!) -> Void in
+               print("Reset")
+           })
+
+           alert.addAction(cancelAction)
+           alert.addAction(defaultAction1)
+           alert.addAction(defaultAction2)
+        
+        present(alert, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
         // 画面背景色を設定してみました
         //self.view.backgroundColor = UIColor(red:0.9,green:1.0,blue:0.9,alpha:1.0)
-    }
-    
-    @IBAction func startTimer(_ sender : Any) {
-        if timer != nil{
-            // timerが起動中なら一旦破棄する
-            timer.invalidate()
-        }
-        
-        timer = Timer.scheduledTimer(
-            timeInterval: 0.01,
-            target: self,
-            selector: #selector(self.timerCounter),
-            userInfo: nil,
-            repeats: true)
-        
-        startTime = Date()
-    }
-    
-    @IBAction func stopTimer(_ sender : Any) {
-        if timer != nil{
-            timer.invalidate()
-            timerHour.text = "00"
-            timerMinute.text = "00"
-            timerSecond.text = "00"
-            //timerMSec.text = "00"
-        }
-    }
-    
-    @objc func timerCounter() {
-        // タイマー開始からのインターバル時間
-        let currentTime = Date().timeIntervalSince(startTime)
-        
-        // fmod() 余りを計算
-        let minute = (Int)(fmod((currentTime/60), 60))
-        // currentTime/60 の余り
-        let second = (Int)(fmod(currentTime, 60))
-        // floor 切り捨て、小数点以下を取り出して *100
-        //let msec = (Int)((currentTime - floor(currentTime))*100)
-        
-        // %02d： ２桁表示、0で埋める
-        let sMinute = String(format:"%02d", minute)
-        let sSecond = String(format:"%02d", second)
-        //let sMsec = String(format:"%02d", msec)
-        
-        timerMinute.text = sMinute
-        timerSecond.text = sSecond
-        //timerMSec.text = sMsec
-        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
