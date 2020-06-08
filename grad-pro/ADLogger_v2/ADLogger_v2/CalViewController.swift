3b1949bc6880a721e1d2ccf59345c356cb77b182
yuri <memory.instrument@gmail.com>
2020-06-08 10:18:47 +0900

    CalViewControllerをひとまず構築。

---
+++ grad-pro/ADLogger_v2/ADLogger_v2/CalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalViewController.swift
+//  ADLogger_v2
+//
+//  Created by 助川友理 on 2020/06/08.
+//  Copyright © 2020 助川友理. All rights reserved.
+//
+
+import UIKit
+
+class CalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
