b7edfea32c156313e75b6f82be577c73345899d5
yuri <memory.instrument@gmail.com>
2020-06-19 21:50:26 +0900

    サインイン関係の整理をした。表面作り直しただけ。

---
+++ grad-pro/ADLogger_v2/ADLogger_v2/SignViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignViewController.swift
+//  ADLogger_v2
+//
+//  Created by 助川友理 on 2020/06/19.
+//  Copyright © 2020 助川友理. All rights reserved.
+//
+
+import UIKit
+
+class SignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
