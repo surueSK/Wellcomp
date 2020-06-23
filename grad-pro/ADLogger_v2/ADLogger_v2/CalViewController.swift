5af733e270f4df36ba1a02b52393796c64369dba
yuri <memory.instrument@gmail.com>
2020-06-23 10:13:31 +0900

    とりまPOSTできたっぽい！！！ヤッホイ、バンザーーイ！

--- grad-pro/ADLogger_v2/ADLogger_v2/CalViewController.swift
+++ grad-pro/ADLogger_v2/ADLogger_v2/CalViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 import Parse
 
 class CalViewController: UIViewController {
-
+    // MARK: - 選択されたテーブルカラムの機能
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // チェックマークを入れる
+        //let cell = tableView.cellForRow(at:indexPath)
+        //cell?.accessoryType = .checkmark
+            // MARK: - 選択されていないテーブルカラムの機能
+        func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+            // チェックマークを外す
+            //let cell = tableView.cellForRow(at:indexPath)
+            //cell?.accessoryType = .none
+           
+        }
+    }
+    // MARK: - 画面表示
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- grad-pro/ADLogger_v2/ADLogger_v2/ListViewController.swift
+++ grad-pro/ADLogger_v2/ADLogger_v2/ListViewController.swift
@@ -95,29 +95,44 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
        
      }
         // MARK: - 選択されたテーブルカラムの機能
+    
+    var parseObject = PFObject(className:"tasktime")
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // チェックマークを入れる
-        //let cell = tableView.cellForRow(at:indexPath)
-        //cell?.accessoryType = .checkmark
-        //ここからアラート
-        let alert: UIAlertController = UIAlertController(title: "保存完了", message: "ご協力有難うございました", preferredStyle:  UIAlertController.Style.alert)
-        let defaultAction1: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
-               (action: UIAlertAction!)  in
-               //アラートが消えるのと画面遷移が重ならないように0.5秒後に画面遷移するようにしてる
-               DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                // 0.5秒後に実行したい処理(計測に戻る)
-                 //self.navigationController?.popViewController(animated: true)
-                }
-           })
-           alert.addAction(defaultAction1)
-        present(alert, animated: true, completion: nil)
+
+        parseObject["username"] = "usernameposttest"
+        parseObject["taskname"] = "tasknameposttest"
+        parseObject["tasktime"] = 1
+        
+        // Saves the new object.
+        parseObject.saveInBackground {
+          (success: Bool, error: Error?) in
+          if (success) {
+            // The object has been saved.
+            //ここからアラート
+            let alert: UIAlertController = UIAlertController(title: "保存完了", message: "ご協力有難うございました", preferredStyle:  UIAlertController.Style.alert)
+            let defaultAction1: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
+                   (action: UIAlertAction!)  in
+                   //アラートが消えるのと画面遷移が重ならないように0.5秒後に画面遷移するようにしてる
+                   DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    // 0.5秒後に実行したい処理(計測に戻る)
+                     //self.navigationController?.popViewController(animated: true)
+                    }
+               })
+               alert.addAction(defaultAction1)
+            self.present(alert, animated: true, completion: nil)
+            
+          } else {
+            print(error as Any)
+            // There was a problem, check error.description
+          }
+        }
+        
+        
         
     }
         // MARK: - 選択されていないテーブルカラムの機能
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        // チェックマークを外す
-        //let cell = tableView.cellForRow(at:indexPath)
-        //cell?.accessoryType = .none
        
     }
     // MARK: - 画面表示
