ea72754f722fbaf3c71304b7884dd03605befc21
yuri <memory.instrument@gmail.com>
2020-06-26 16:42:43 +0900

    継承の練習

--- 2020Spring/jnHW0625/flask_app/app.py
+++ 2020Spring/jnHW0625/flask_app/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  title = 'タイトル'
+  message = '本メッセージ及び下記のリストはapp.pyからindex.htmlに反映させています。'
+  return render_template('index.html', title=title, message=message)
 
 if __name__ == '__main__':
   app.run(debug=True)
