0fbd7dd5b2136a13a0972c4002701130eac86cf4
yuri <memory.instrument@gmail.com>
2020-06-26 17:06:32 +0900

    for文の練習

--- 2020Spring/jnHW0625/flask_app/app.py
+++ 2020Spring/jnHW0625/flask_app/app.py
@@ -7,7 +7,15 @@ def index():
   title = 'タイトル'
   message = '本メッセージ及びリスト、辞書型配列はapp.pyからindex.htmlに反映させています。'
   shikoku_list = ['愛媛県', '高知県', '徳島県', '香川県']
-  return render_template('index.html', title=title, message=message,shikoku=shikoku_list)
+  fruit_dict = {'orange':100, 'apple':200}
+
+  return render_template(
+  'index.html',
+  title=title,
+  message=message,
+  shikoku=shikoku_list,
+  fruit=fruit_dict
+  )
 
 if __name__ == '__main__':
   app.run(debug=True)
