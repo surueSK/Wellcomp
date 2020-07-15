f2c28460a368c868dcd7dbd1fad1d54b48d10b60
yuri <memory.instrument@gmail.com>
2020-07-15 22:53:39 +0900

    一旦テーブルカラムはテスト状態でプッシュ。明日以降考えようかな。

--- grad-pro/ADLogger_v2/ADLogger_v2/CalViewController.swift
+++ grad-pro/ADLogger_v2/ADLogger_v2/CalViewController.swift
@@ -9,52 +9,51 @@
 import UIKit
 import Parse
 
-class CalViewController: UIViewController {
+class CalViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
         // MARK: - Table基本設定
     @IBOutlet var tableView : UITableView!
-
-    // テーブルに表示するデータの準備
-    var calItem : [String] = []
     
-    // テーブルの行数を指定
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calItem.count
-    }
-    // セルの中身を設定
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // セルを取得する
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "calCell", for: indexPath)
-        // セルに値を設定する
-        cell.textLabel!.text = calItem[indexPath.row]
-        return cell
-       
-    }
-
-    // MARK: - 選択されたテーブルカラムの機能
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        // チェックマークを入れる
-        let cell = tableView.cellForRow(at:indexPath)
-        cell?.accessoryType = .checkmark
-    }
-            // MARK: - 選択されていないテーブルカラムの機能
-        func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-            // チェックマークを外す
-        let cell = tableView.cellForRow(at:indexPath)
-        cell?.accessoryType = .none
-           
-        }
-    // MARK: - 画面表示
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            if UserDefaults.standard.object(forKey: "mycell") != nil{
-                calItem = UserDefaults.standard.object(forKey: "mycell") as! [String]
-            }
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
             
-            tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "timercell")
         }
         
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // 一つのsectionの中に入れるCellの数を決める。
+            
+            return 50
+        }
+        
+        
+        func numberOfSections(in tableView: UITableView) -> Int { // sectionの数を決める
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            // Cellの高さを決める
+            
+            return 50
+        }
+        
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            // Cellの内容を決める（超重要）
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "calCell", for: indexPath)
+            //ここで先ほど指定した『beginnerCell』を呼んでる。
+            cell.textLabel?.text = "swift"
+            
+            return cell
+            
+        }
 
         // Do any additional setup after loading the view.
     }
