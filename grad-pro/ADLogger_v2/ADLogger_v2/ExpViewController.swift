cebfb9848ca3799b6c7f195669394428d97c211f
yuri <memory.instrument@gmail.com>
2020-08-24 21:58:26 +0900

    アンケート用のExpViewを作った。みためも少し変えた。

---
+++ grad-pro/ADLogger_v2/ADLogger_v2/ExpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ExpViewController.swift
+//  ADLogger_v2
+//
+//  Created by 助川友理 on 2020/08/24.
+//  Copyright © 2020 助川友理. All rights reserved.
+//
+
+import UIKit
+
+class ExpViewController: UIViewController {
+    
+    @IBOutlet weak var etname: UITextField!
+    @IBOutlet weak var ehour: UITextField!
+    @IBOutlet weak var emin: UITextField!
+    @IBOutlet weak var esec: UITextField!
+    @IBOutlet weak var ebutton: UIButton!
+    
+    @IBAction func eOK(_ sender: Any) {
+       
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
